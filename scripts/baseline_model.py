4ef7e1244e0e4c26a0a5f09ba55c964913e43db6
vek239 <vlasova.elizaveta@yandex.ru>
2021-07-01 11:36:28 +0300

    Added molecule representation and dataset basic analysis

---
+++ scripts/baseline_model.py
@@ -0,0 +1,27 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Descriptors
+from rdkit.ML.Descriptors import MoleculeDescriptors
+
+
+class MoleculeModel:
+    def calc_descriptors(self):
+        calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in Descriptors._descList])
+        self.header = calc.GetDescriptorNames()
+        self.descriptors = dict(zip(self.header, calc.CalcDescriptors(self.mol)))
+
+    def calc_fingerprint(self, count=True, radius=2, bits=1024):
+        if count:
+            self.fingerprint = AllChem.GetMorganFingerprint(self.mol, radius)
+        else:
+            self.fingerprint = AllChem.GetMorganFingerprintAsBitVect(self.mol, radius, nBits=bits)
+
+    def __init__(self, smiles, count=True, radius=2, bits=1024):
+        self.smiles = smiles
+        self.mol = Chem.MolFromSmiles(smiles)
+        self.calc_descriptors()
+        self.calc_fingerprint(count, radius, bits)
+
+    def get_mol_representation(self):
+        return {'descriptors': self.descriptors,
+                'fingerprint': self.fingerprint}
