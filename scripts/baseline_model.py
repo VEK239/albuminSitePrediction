0ee840dfadeadd059b6442a8e64e75657f21a819
vek239 <vlasova.elizaveta@yandex.ru>
2021-07-02 17:38:41 +0300

    Added small dataset eda, changed representation

--- scripts/baseline_model.py
+++ scripts/baseline_model.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import Descriptors
@@ -10,13 +11,13 @@ class MoleculeModel:
         self.header = calc.GetDescriptorNames()
         self.descriptors = dict(zip(self.header, calc.CalcDescriptors(self.mol)))
 
-    def calc_fingerprint(self, count=True, radius=2, bits=1024):
+    def calc_fingerprint(self, count=True, radius=2, bits=64):
         if count:
             self.fingerprint = AllChem.GetMorganFingerprint(self.mol, radius)
         else:
             self.fingerprint = AllChem.GetMorganFingerprintAsBitVect(self.mol, radius, nBits=bits)
 
-    def __init__(self, smiles, count=True, radius=2, bits=1024):
+    def __init__(self, smiles, count=True, radius=2, bits=64):
         self.smiles = smiles
         self.mol = Chem.MolFromSmiles(smiles)
         self.calc_descriptors()
@@ -25,3 +26,6 @@ class MoleculeModel:
     def get_mol_representation(self):
         return {'descriptors': self.descriptors,
                 'fingerprint': self.fingerprint}
+
+    def get_mol_representation_as_series(self):
+        return pd.Series(list(self.descriptors.values()) + self.fingerprint)
