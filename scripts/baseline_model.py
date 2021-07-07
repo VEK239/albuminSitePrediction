392419ed43a3bf2510c072fe637e7850cc700ca1
vek239 <vlasova.elizaveta@yandex.ru>
2021-07-07 10:49:27 +0300

    Updated models for rdkit and qikprop

--- scripts/baseline_model.py
+++ scripts/baseline_model.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -5,32 +7,51 @@ from rdkit.Chem import Descriptors
 from rdkit.ML.Descriptors import MoleculeDescriptors
 import padelpy as pdp
 
+with open('../data/qikprop/qikprop.json') as ouf:
+    qikprop_dict = json.loads(ouf.read())
+
 class MoleculeModel:
     def calc_descriptors_rdkit(self):
         calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in Descriptors._descList])
         self.header = calc.GetDescriptorNames()
-        self.descriptors = dict(zip(self.header, calc.CalcDescriptors(self.mol)))
+        self.rdkit_descriptors = dict(zip(self.header, calc.CalcDescriptors(self.mol)))
+
+    def calc_descriptors_qikprop(self):
+        try:
+            self.qikprop_descriptors = qikprop_dict[self.smiles]
+        except Exception as e:
+            self.qikprop_descriptors = None
 
     def calc_descriptors_padel(self):
-        self.padel_descriptors = pdp.from_smiles(self.smiles)
+        try:
+            self.padel_descriptors = pdp.from_smiles(self.smiles)
+        except Exception as e:
+            self.padel_descriptors = None
 
-    def calc_fingerprint(self, count=True, radius=2, bits=64):
-        if count:
-            self.fingerprint = AllChem.GetMorganFingerprint(self.mol, radius)
-        else:
-            self.fingerprint = AllChem.GetMorganFingerprintAsBitVect(self.mol, radius, nBits=bits)
+    def calc_fingerprint(self, radius=2, bits=64):
+        self.fingerprint = AllChem.GetMorganFingerprintAsBitVect(self.mol, radius, nBits=bits)
 
-    def __init__(self, smiles, count=True, radius=2, bits=64):
+    def __init__(self, smiles, radius=2, bits=64, padel=False, rdkit=True, qikprop=True, fp=True):
         self.smiles = smiles
         self.mol = Chem.MolFromSmiles(smiles)
-        self.calc_descriptors_padel()
-        self.calc_descriptors_rdkit()
-        self.calc_fingerprint(count, radius, bits)
+        self.padel_descriptors = None
+        self.qikprop_descriptors = None
+        self.fingerprint = None
+        self.rdkit_descriptors = None
+        if padel:
+            self.calc_descriptors_padel()
+        if rdkit:
+            self.calc_descriptors_rdkit()
+        if qikprop:
+            self.calc_descriptors_qikprop()
+        if fp:
+            self.calc_fingerprint(radius, bits)
 
     def get_mol_representation(self):
-        return {'descriptors_rdkit': self.descriptors,
+        return {'descriptors_rdkit': self.rdkit_descriptors,
                 'descriptors_padel': self.padel_descriptors,
+                'descriptors_qikprop': self.qikprop_descriptors,
                 'fingerprint': self.fingerprint}
 
     def get_mol_representation_as_series(self):
-        return pd.Series(list(self.descriptors.values()) + self.fingerprint)
+        return pd.Series(list(self.rdkit_descriptors.values()) + self.fingerprint)
